5e1f7006ec203b1c8a96f4a048f994f5757021b4
jamesparkes <parkesrjames@gmail.com>
2016-03-09 14:45:36 -0600

    finish specifications before turning in project

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	$('#my-icon').css('height', $('#my-name').height());
+
 	$('.project').each(function() {
 	    var link = $(this).find('a').attr('href');
 	    $(this).find('.project-image').wrap('<a href="' + link + '" target="_blank"></a>');
